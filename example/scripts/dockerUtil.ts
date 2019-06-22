1f011d591e3d306576727aab420c58d5510278d1
Nick Rook <Nick_Rook@intuit.com>
2019-06-21 23:37:10 -0700

    added dockerUtil script

---
+++ example/scripts/dockerUtil.ts
@@ -0,0 +1,83 @@
+/* eslint no-console:0 */
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+import * as parseArgs from "minimist";
+import * as spawn from "cross-spawn";
+import { name, version } from "../package.json";
+
+const IMAGE_NAME = `rooknj/${name}`;
+
+const getDockerImage = (tag: string): string => {
+  let dockerTag = tag;
+  if (process.env.TRAVIS) {
+    console.log("In CI");
+    const tagName = process.env.TRAVIS_TAG;
+    if (tagName) {
+      dockerTag = version;
+    } else {
+      const branchName = process.env.TRAVIS_BRANCH;
+      if (branchName === "master") {
+        dockerTag = "master";
+      } else {
+        dockerTag = "test";
+      }
+    }
+  }
+  return `${IMAGE_NAME}:${dockerTag}`;
+};
+
+const buildDockerImage = async (tag: string): Promise<void> => {
+  const image = getDockerImage(tag);
+  console.log("Building", image);
+  const child = spawn("docker", ["build", "-t", image, "."], {
+    stdio: ["inherit", "inherit", "inherit"],
+    cwd: process.cwd(),
+    env: process.env,
+  });
+
+  // If docker build fails, then exit with an error code
+  child.on(
+    "exit",
+    (code: number): void => {
+      if (code !== 0) process.exit(code);
+    }
+  );
+};
+
+const publishDockerImage = async (tag: string): Promise<void> => {
+  const image = getDockerImage(tag);
+  console.log("Publishing", image);
+  const child = spawn("docker", ["push", image], {
+    stdio: ["inherit", "inherit", "inherit"],
+    cwd: process.cwd(),
+    env: process.env,
+  });
+
+  // If docker push fails, then exit with an error code
+  child.on(
+    "exit",
+    (code: number): void => {
+      if (code !== 0) process.exit(code);
+    }
+  );
+};
+
+// Process all command line arguments
+const processArgs = async (argv: string[]): Promise<void> => {
+  const args = parseArgs(argv.slice(2));
+  // a tag is required if not in CI
+  if (!args.t && !process.env.TRAVIS) {
+    console.log("Not in CI and no tag was given. Aborting");
+    process.exit(1);
+  }
+
+  if (args._.find((arg: string): boolean => arg === "build")) {
+    buildDockerImage(args.t);
+  } else if (args._.find((arg: string): boolean => arg === "publish")) {
+    publishDockerImage(args.t);
+  } else {
+    console.log("No valid options supplied. Aborting");
+    process.exit(1);
+  }
+};
+
+processArgs(process.argv);
---
+++ example/x86.Dockerfile
@@ -0,0 +1,37 @@
+## Build Environment
+FROM node:10 as builder
+
+# Create app directory
+WORKDIR /usr/app
+
+# Add app
+COPY . .
+
+# install deps
+RUN yarn install
+
+# Test app
+RUN yarn test --no-watch
+
+# Install pkg and Build App
+RUN yarn build
+
+## Prod Environment
+FROM alpine:latest
+
+WORKDIR /usr/app
+
+# Start QEMU support for building on all architectures
+RUN [ "cross-build-start" ]
+RUN mkdir data
+RUN [ "cross-build-end" ]
+
+COPY --from=builder /usr/app/build/example-ts /usr/app
+# Note: We are including the sqlite native module addon here so zeit/pkg can use it
+# Native modules can't be included in the executable but can be included in the same directory
+COPY --from=builder /usr/app/node_modules/sqlite3/lib/binding/node-v64-linux-arm/node_sqlite3.node /usr/app
+# Make port 4001 available to the world outside this container
+EXPOSE 4001
+
+# Start the app
+ENTRYPOINT ["./example-ts"]
