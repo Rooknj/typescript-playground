8b9b3c5436f6a053d3fc70bffa23573e5deeba19
Nick Rook <Nick_Rook@intuit.com>
2019-06-21 20:31:15 -0700

    ran prettier

--- templateServer/src/modules/user/UserResolver.ts
+++ templateServer/src/modules/user/UserResolver.ts
@@ -1,114 +1,129 @@
-import { Arg, Resolver, Query, Authorized, Mutation, Ctx, ID, InputType, Field } from 'type-graphql'
-import { Context } from '../common/context'
-import { UserService } from './UserService'
-import { User, Profile } from './UserEntity'
-import './enums'
-import { accountsPassword } from './accounts'
-import { Role } from './consts'
-import { client as plaid } from '../payments/plaid'
+import {
+  Arg,
+  Resolver,
+  Query,
+  Authorized,
+  Mutation,
+  Ctx,
+  ID,
+  InputType,
+  Field
+} from "type-graphql";
+import { Context } from "../common/context";
+import { UserService } from "./UserService";
+import { User, Profile } from "./UserEntity";
+import "./enums";
+import { accountsPassword } from "./accounts";
+import { Role } from "./consts";
+import { client as plaid } from "../payments/plaid";
 
 @InputType()
 class ProfileInput implements Partial<Profile> {
   @Field(type => String)
-  public firstName: string
+  public firstName: string;
 
   @Field(type => String)
-  public lastName: string
+  public lastName: string;
 }
 
 @InputType()
 class CreateUserInput implements Partial<User> {
   @Field(type => String)
-  email: string
+  email: string;
 
   @Field(type => String)
-  password: string
+  password: string;
 
   @Field(type => ProfileInput)
-  profile: ProfileInput
+  profile: ProfileInput;
 }
 
 @InputType()
 export class PropertyInput {
   @Field(type => String)
-  address: string
+  address: string;
 
   @Field(type => String)
-  placeId: string
+  placeId: string;
 
   @Field(type => Number)
-  rentAmount: number
+  rentAmount: number;
 }
 
 @Resolver(User)
 export default class UserResolver {
-  private readonly service: UserService
+  private readonly service: UserService;
 
   constructor() {
-    this.service = new UserService()
+    this.service = new UserService();
   }
 
   @Query(returns => User)
   @Authorized()
   async me(@Ctx() ctx: Context) {
     if (ctx.userId) {
-      return await this.service.findOneById(ctx.userId)
+      return await this.service.findOneById(ctx.userId);
     }
   }
 
   // this overrides accounts js `createUser` function
   @Mutation(returns => ID)
-  public async createUser(@Arg('user', returns => CreateUserInput) user: CreateUserInput) {
+  public async createUser(
+    @Arg("user", returns => CreateUserInput) user: CreateUserInput
+  ) {
     const createdUserId = await accountsPassword.createUser({
       ...user,
-      roles: [Role.User],
-    })
+      roles: [Role.User]
+    });
 
-    return createdUserId
+    return createdUserId;
   }
 
   @Mutation(returns => Boolean)
   @Authorized()
   public async onboardUser(
-    @Arg('publicToken') publicToken: string,
-    @Arg('property') property: PropertyInput,
+    @Arg("publicToken") publicToken: string,
+    @Arg("property") property: PropertyInput,
     @Ctx() ctx: Context
   ) {
     return new Promise((resolve, reject) => {
       plaid.exchangePublicToken(publicToken, async (err, response) => {
-        if (err != null) reject(err)
+        if (err != null) reject(err);
 
-        const user = await this.service.findOneById(ctx.userId)
+        const user = await this.service.findOneById(ctx.userId);
         user.plaid = {
           accessToken: response.access_token,
-          itemId: response.item_id,
-        }
-        user.properties = [property]
-        user.isOnboarded = true
-        await user.save()
+          itemId: response.item_id
+        };
+        user.properties = [property];
+        user.isOnboarded = true;
+        await user.save();
 
-        resolve(true)
-      })
-    })
+        resolve(true);
+      });
+    });
   }
 
   @Mutation(returns => Boolean)
   @Authorized()
-  public async setPlaidToken(@Arg('publicToken') publicToken: string, @Ctx() ctx: Context) {
+  public async setPlaidToken(
+    @Arg("publicToken") publicToken: string,
+    @Ctx() ctx: Context
+  ) {
     return new Promise((resolve, reject) => {
       plaid.exchangePublicToken(publicToken, async (err, response) => {
-        if (err != null) reject(err)
+        if (err != null) reject(err);
 
-        const user = await this.service.findOneById(ctx.userId)
+        const user = await this.service.findOneById(ctx.userId);
         user.plaid = {
           accessToken: response.access_token,
-          itemId: response.item_id,
-        }
-        await user.save()
+          itemId: response.item_id
+        };
+        await user.save();
 
-        resolve(true)
-      })
-    })
+        resolve(true);
+      });
+    });
   }
 
   // @FieldResolver(returns => String)
