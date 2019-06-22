9e4ada488008a80e9e6603e856a99842660c03c7
Nick Rook <Nick_Rook@intuit.com>
2019-06-21 22:19:20 -0700

    added a test script

---
+++ example/scripts/test.ts
@@ -0,0 +1,36 @@
+/* eslint no-console:0 */
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+import * as spawn from "cross-spawn";
+
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = "test";
+process.env.NODE_ENV = "test";
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on(
+  "unhandledRejection",
+  (err): never => {
+    throw err;
+  }
+);
+
+const argv = process.argv.slice(2);
+
+if (argv.indexOf("--no-watch") >= 0) {
+  process.env.CI = "true";
+  argv.splice(argv.indexOf("--no-watch"), 1); // Remove no-watch option from argv so jest doesnt screw up
+}
+
+// Watch unless on CI or in coverage mode
+if (!process.env.CI && argv.indexOf("--coverage") < 0) {
+  argv.push("--watch");
+}
+
+const { status } = spawn.sync("jest", argv, {
+  stdio: ["inherit", "inherit", "inherit"],
+});
+
+// If any tests failed, return with an error exit code
+if (status !== 0) process.exit(status);
