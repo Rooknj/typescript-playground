fb48aa94a7d24a64808fcf1d4fd861a0a87003c0
Nick Rook <Nick_Rook@intuit.com>
2019-06-21 23:05:53 -0700

    added clean script

---
+++ example/scripts/clean.ts
@@ -0,0 +1,33 @@
+/* eslint no-console:0 */
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+import * as rimraf from "rimraf";
+// import { execSync } from "child_process";
+
+const remove = (fileOrDirectory: string): void => {
+  rimraf(
+    fileOrDirectory,
+    (error: Error): void => {
+      if (error) {
+        console.log(`Error removing ${fileOrDirectory} ❌: `, error);
+      } else {
+        console.log(`Successfully removed ${fileOrDirectory} ✅`);
+      }
+    }
+  );
+};
+
+// Remove dist folder
+remove("dist");
+
+// Remove build folder
+remove("build");
+
+// Remove coverage folder
+remove("coverage");
+
+// Remove database
+// remove("data/*.sqlite");
+
+// Bring down docker containers
+// console.log("Bringing docker containers down");
+// execSync("docker-compose down");
