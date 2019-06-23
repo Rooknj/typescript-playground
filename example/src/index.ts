d1eaead6a5d3e385f1dc6672179eb3a71b501dce
Nick Rook <Nick_Rook@intuit.com>
2019-06-23 14:51:35 -0700

    added simple apollo server

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,6 +1,30 @@
-import * as path from "path";
+/* eslint no-console:0 */
+import { ApolloServer, gql } from "apollo-server";
 
+// Wrap index.js inside an immediately invoked async function
 (async (): Promise<void> => {
   console.log("Hello World");
-  console.log(path.delimiter);
+
+  // The GraphQL schema
+  const typeDefs = gql`
+    type Query {
+      "A simple type for getting started!"
+      hello: String
+    }
+  `;
+
+  // A map of functions which return data for the schema.
+  const resolvers = {
+    Query: {
+      hello: (): string => "world",
+    },
+  };
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  const { url } = await server.listen();
+  console.log(`🚀 Server ready at ${url}`);
 })();
