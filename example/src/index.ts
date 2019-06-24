e279328def780cc3802f7528bae8a1035baefcbe
Nick Rook <Nick_Rook@intuit.com>
2019-06-23 18:19:47 -0700

    moved lights to its own folder

--- example/src/index.ts
+++ example/src/index.ts
@@ -3,13 +3,10 @@ import "reflect-metadata";
 import path from "path";
 import { ApolloServer } from "apollo-server";
 import { buildSchema } from "type-graphql";
-import { LightResolver } from "./light-resolver";
+import { LightResolver } from "./Lights/light-resolver";
 
 // Wrap index.js inside an immediately invoked async function
 (async (): Promise<void> => {
-  console.log(process.env.NODE_ENV);
-  console.log("Hello World");
-
   // build TypeGraphQL executable schema
   const schema = await buildSchema({
     resolvers: [LightResolver],
